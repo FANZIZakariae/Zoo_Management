4f3f8789f05a3b9ba660306a2ce0fe829308413b
e2405354 <zakariae_fanzi@um5.ac.ma>
2024-11-29 11:43:18 +0100

    Second Commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class App {
+
+	private static Zoo zoo;
+	
+	public static void main(String[] args) {
+		zoo = new Zoo();
+		System.out.println("Test");
+	}
+	
+	public static Zoo getZoo() {
+		return zoo;
+	}
+}
---
+++ src/main/java/org/example/Chat.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Chat extends Animal {
+
+	public Chat(String nomAnimal, TypeAnimal typeAnimal) {
+		super(nomAnimal, typeAnimal);
+	}
+
+	@Override
+	public String getNomAnimal() {
+		return "Le Chat " + super.getNomAnimal();
+	}
+}
